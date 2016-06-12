9caff1b59d091dd05cfe075b95f410e2c7b9f7cf
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-06-12 15:38:02 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# nrafund
+
+Many of our representatives are being funded by the NRA, don't support
+background checks, and aren't taking action against gun violence.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>NRA Funded</h1>
+        <p>
+          Many of our representatives are being funded by the NRA, don't support
+          background checks, and aren't taking action against gun violence.
+        </p>
+      </main>
+    )
+  }
+}
