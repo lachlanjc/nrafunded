18287d24f6214510b232d357d7de01ec90d8bc66
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-10-10 22:33:33 -0400

    Clean up Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Box } from 'rebass'
+import { Box, Link } from 'rebass'
 
-const FooterLink = ({ unpadLeft, unpadRight, ...props }) => (
-  <Box
-    is="a"
+const FooterLink = props => (
+  <Link
     style={{
       textDecoration: 'none',
-      paddingLeft: unpadLeft ? null : 4,
-      paddingRight: unpadRight ? null : 4
+      paddingLeft: 4,
+      paddingRight: 4
     }}
     {...props}
   />
@@ -16,70 +15,22 @@ const FooterLink = ({ unpadLeft, unpadRight, ...props }) => (
 const Footer = () => (
   <Box
     is="footer"
-    px={1}
-    py={3}
-    mt={3}
+    px={2}
+    py={4}
+    bg="gray2"
+    color="midgray"
     style={{
-      borderTop: '1px solid #dae4eb',
-      backgroundColor: '#f4f7f9',
-      color: '#566b7b',
       textAlign: 'center'
     }}
   >
     <section>
       Created by
-      <FooterLink href="https://lachlanjc.me/" children="@lachlanjc" />
-      in 2016 –
+      <FooterLink href="https://lachlanjc.glitch.me/" children="@lachlanjc" />
+      in 2017 –
       <FooterLink
         href="https://github.com/lachlanjc/nrafunded/"
         children="Contribute on GitHub"
-        unpadRight
       />
-    </section>
-    <section style={{ fontSize: 14 }}>
-      Built with
-      <FooterLink
-        href="https://facebook.github.io/react/"
-        children="react"
-        unpadRight
-      />
-      ,
-      <FooterLink
-        href="https://github.com/insin/nwb"
-        children="nwb"
-        unpadRight
-      />
-      ,
-      <FooterLink
-        href="http://jxnblk.com/rebass"
-        children="rebass"
-        unpadRight
-      />
-      ,
-      <FooterLink
-        href="http://jxnblk.com/react-geomicons/"
-        children="geomicons"
-        unpadRight
-      />
-      ,
-      <FooterLink
-        href="https://github.com/jxnblk/static-react"
-        children="static-react"
-        unpadRight
-      />
-      ,
-      <FooterLink
-        href="https://npmjs.com/package/axios"
-        children="axios"
-        unpadRight
-      />
-      , &
-      <FooterLink
-        href="https://github.com/IonicaBizau/scrape-it"
-        children="scrape-it"
-        unpadRight
-      />
-      .
     </section>
   </Box>
 )
